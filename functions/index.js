49c3b13960a0975a2e9d811c79d0db43402b67bb
andrew <burns.andrew@hotmail.co.uk>
2019-07-15 16:45:02 +0100

    initial commit

--- functions/index.js
+++ functions/index.js
@@ -1,205 +1,221 @@
-// //
-// // DemoUnidosXFIS
-//
-//    Theme  : Firebase & Face Recognition on Real Time
-//    Author : Luis Uculmana Lara
-//             Full Stack Developer & GDG Lead
-//    Email  : ithecode.ica@gmail.com
-//
-// //
-const tfjs = require('@tensorflow/tfjs-node')
-const firebase = require('firebase')
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
-const faceapi = require('face-api.js')
-const stream = require('stream')
-const Buffer = require('buffer/').Buffer
-const canvas = require('canvas')
-const { Canvas, Image, ImageData } = canvas
+const tfjs = require("@tensorflow/tfjs-node");
+const firebase = require("firebase");
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const faceapi = require("face-api.js");
+const stream = require("stream");
+const Buffer = require("buffer/").Buffer;
+const canvas = require("canvas");
+const { Canvas, Image, ImageData } = canvas;
 
 // -- Configuration Settings for Firebase and Firebase-admin --
 
-  // patch nodejs environment, we need to provide an implementation of
-  // HTMLCanvasElement and HTMLImageElement, additionally an implementation
-  // of ImageData is required, in case you want to use the MTCNN
-  faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
+// patch nodejs environment, we need to provide an implementation of
+// HTMLCanvasElement and HTMLImageElement, additionally an implementation
+// of ImageData is required, in case you want to use the MTCNN
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 
-  // Get This credential from https://console.cloud.google.com/apis/credentials/serviceaccountkey
-  const serviceAccount = require("./keys/serviceaccount.json")
+// Get This credential from https://console.cloud.google.com/apis/credentials/serviceaccountkey
+const serviceAccount = require("./keys/serviceaccount.json");
 
-  // File for load models from Disk, if you use the same models, dont touch it.
-  const MODEL_DIR = './models'
+// File for load models from Disk, if you use the same models, dont touch it.
+const MODEL_DIR = "./models";
 
-  // Initialize Firebase-admin
-  admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      storageBucket: "demounidosxfis.appspot.com",
-      databaseURL: "https://demounidosxfis.firebaseio.com"
-  })
+// Initialize Firebase-admin
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "find-my-face.appspot.com",
+  databaseURL: "https://find-my-face.firebaseio.com"
+});
 
-  // Setting on the timestamps for the documents.
-  admin.firestore().settings({ timestampsInSnapshots: true })
+// Setting on the timestamps for the documents.
+admin.firestore().settings({ timestampsInSnapshots: true });
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyACYKQnJF1ofSqzhkC-D0adISQX2hZ2EBc",
-    authDomain: "demounidosxfis.firebaseapp.com",
-    databaseURL: "https://demounidosxfis.firebaseio.com",
-    projectId: "demounidosxfis",
-    storageBucket: "demounidosxfis.appspot.com",
-    messagingSenderId: "519004356342",
-    appId: "1:519004356342:web:5973887e27258c23"
-  }
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCMhyzLJpAt6KmCeEgnT9FV4niLUqaP7fw",
+  authDomain: "find-my-face.firebaseapp.com",
+  databaseURL: "https://find-my-face.firebaseio.com",
+  projectId: "find-my-face",
+  storageBucket: "find-my-face.appspot.com",
+  messagingSenderId: "342095524666",
+  appId: "1:342095524666:web:3de5759e04626187"
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig)
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 // -- Configuration Settings for Firebase and Firebase-admin --
-
-
-
-
-
 
 // // Functions to be exported.
 // // Read more at https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.processor = functions.https.onCall(async (data, context) => {
-
   // Load 3 models from the disk.
-  await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_DIR)
-  await faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_DIR)
-  await faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_DIR)
+  await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_DIR);
+  await faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_DIR);
+  await faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_DIR);
 
   // Load all images from firestore reusing the another function with null data.
-  var labels = admin.firestore().collection('images').get()
-                  .then(snap => snap.docs.map(doc => {
-                      var obj = doc.data()
-                      obj.key = doc.id
-                      return obj
-                  }))
-                  .catch(e => e)
+  var labels = admin
+    .firestore()
+    .collection("images")
+    .get()
+    .then(snap =>
+      snap.docs.map(doc => {
+        var obj = doc.data();
+        obj.key = doc.id;
+        return obj;
+      })
+    )
+    .catch(e => e);
 
   const labeledFaceDescriptors = await Promise.all(
     labels.map(async label => {
       // fetch image data from urls and convert blob to HTMLImage element
-      const img = await canvas.loadImage(label.image_original)
+      const img = await canvas.loadImage(label.image_original);
 
       // detect the face with the highest score in the image and compute it's landmarks and face descriptor
-      const fullFaceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+      const fullFaceDescription = await faceapi
+        .detectSingleFace(img)
+        .withFaceLandmarks()
+        .withFaceDescriptor();
 
       // If we dont detect any face we'll show a error.
       if (!fullFaceDescription) {
-        throw new Error(`no faces detected for ${label}`)
+        throw new Error(`no faces detected for ${label}`);
       }
 
       // Returning for the map function data.
       return {
-          descriptor : [fullFaceDescription.descriptor],
-          label : label.name
-      }
-
+        descriptor: [fullFaceDescription.descriptor],
+        label: label.name
+      };
     })
-  )
+  );
 
   // Returning all data maped.
-  return labeledFaceDescriptors
-
-})
-
+  return labeledFaceDescriptors;
+});
 
 exports.savingPhoto = functions.https.onCall(async (data, context) => {
-
   // Load 3 models from the disk
-  await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_DIR)
-  await faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_DIR)
-  await faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_DIR)
+  await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_DIR);
+  await faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_DIR);
+  await faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_DIR);
 
   // In NodeJS can't process directly the image, that's because we'll use firebase storage for photos.
   const uploadPicture = function(base64, label) {
     return new Promise((resolve, reject) => {
       if (!base64 || !label) {
-        reject("Could not upload picture because at least one param is missing.")
+        reject(
+          "Could not upload picture because at least one param is missing."
+        );
       }
 
-      let bufferStream = new stream.PassThrough()
-      var timestamp = new Date().getTime()
-      bufferStream.end(new Buffer.from(base64, 'base64'))
+      let bufferStream = new stream.PassThrough();
+      var timestamp = new Date().getTime();
+      bufferStream.end(new Buffer.from(base64, "base64"));
 
       // Retrieve default storage bucket
-      let bucket = admin.storage().bucket()
+      let bucket = admin.storage().bucket();
 
-      var file_name = `${label}_${timestamp}.png`
-      var bucket_name = "demounidosxfis.appspot.com"
+      var file_name = `${label}_${timestamp}.png`;
+      var bucket_name = "demounidosxfis.appspot.com";
 
       // Create a reference to the new image file
-      let file = bucket.file(file_name)
+      let file = bucket.file(file_name);
 
-      bufferStream.pipe(file.createWriteStream({
-        metadata: {
-          contentType: 'image/png'
-        }
-      }))
-      .on('error', error => {
-        reject(`Error while uploading picture ${JSON.stringify(error)}`)
-      })
-      .on('finish', (file) => {
-        // We need resolving with the url of png.
-        var downloadUrl = "https://firebasestorage.googleapis.com/v0/b/" + bucket_name + "/o/" + file_name + "?alt=media"
-        resolve(downloadUrl)
-
-      })
-    })
-  }
+      bufferStream
+        .pipe(
+          file.createWriteStream({
+            metadata: {
+              contentType: "image/png"
+            }
+          })
+        )
+        .on("error", error => {
+          reject(`Error while uploading picture ${JSON.stringify(error)}`);
+        })
+        .on("finish", file => {
+          // We need resolving with the url of png.
+          var downloadUrl =
+            "https://firebasestorage.googleapis.com/v0/b/" +
+            bucket_name +
+            "/o/" +
+            file_name +
+            "?alt=media";
+          resolve(downloadUrl);
+        });
+    });
+  };
 
   // I need to convert the base64 to firebase storage url bucket.
-  var image_original_url = await uploadPicture(data.photo.replace(/^data:image\/png;base64,/, ""), data.label)
+  var image_original_url = await uploadPicture(
+    data.photo.replace(/^data:image\/png;base64,/, ""),
+    data.label
+  );
 
   // Loading image original from url.
-  const image_original = await canvas.loadImage(image_original_url)
+  const image_original = await canvas.loadImage(image_original_url);
 
   // Detecting all faces.
-  let fullFaceDescriptions = await faceapi.detectAllFaces(image_original).withFaceLandmarks().withFaceDescriptors()
+  let fullFaceDescriptions = await faceapi
+    .detectAllFaces(image_original)
+    .withFaceLandmarks()
+    .withFaceDescriptors();
 
   // Creating a Canvas element for draw the faces.
-  const image_process = faceapi.createCanvasFromMedia(image_original)
+  const image_process = faceapi.createCanvasFromMedia(image_original);
 
   // Drawing Box & Landmarks
-  faceapi.draw.drawDetections(image_process, fullFaceDescriptions, { lineWidth: 4, color: 'blue' })
-  faceapi.draw.drawFaceLandmarks(image_process, fullFaceDescriptions, { lineWidth: 4, color: 'red' })
+  faceapi.draw.drawDetections(image_process, fullFaceDescriptions, {
+    lineWidth: 4,
+    color: "blue"
+  });
+  faceapi.draw.drawFaceLandmarks(image_process, fullFaceDescriptions, {
+    lineWidth: 4,
+    color: "red"
+  });
 
   // Getting base64 from new canvas processed.
-  var dataURI = image_process.toDataURL('image/png')
+  var dataURI = image_process.toDataURL("image/png");
 
   // Then we need to convert base64 to firebase storage url bucket.
-  var image_landmark_url = await uploadPicture(dataURI.replace(/^data:image\/png;base64,/, ""), data.label)
-
+  var image_landmark_url = await uploadPicture(
+    dataURI.replace(/^data:image\/png;base64,/, ""),
+    data.label
+  );
 
   // Label name, original screenshot and image landmarked.
   var data = {
-    'label' : data.label,
-    'image_original' : image_original_url,
-    'image_landmark' : image_landmark_url
-  }
+    label: data.label,
+    image_original: image_original_url,
+    image_landmark: image_landmark_url
+  };
 
   // Saving the image in Firestore Realtime
-  await admin.firestore().collection('images').add(data)
+  await admin
+    .firestore()
+    .collection("images")
+    .add(data);
 
   // Returning data for be add on latest screenshot
-  return data
-
-})
+  return data;
+});
 
 exports.loadPhotos = functions.https.onCall(async (data, context) => {
-
   // Returning all images from the collection parsed as object.
-  return admin.firestore().collection('images').get()
-                .then(snap => snap.docs.map(doc => {
-                    var obj = doc.data()
-                    obj.key = doc.id
-                    return obj
-                }))
-                .catch(e => e)
-
-
-})
+  return admin
+    .firestore()
+    .collection("images")
+    .get()
+    .then(snap =>
+      snap.docs.map(doc => {
+        var obj = doc.data();
+        obj.key = doc.id;
+        return obj;
+      })
+    )
+    .catch(e => e);
+});
--- public/js/app.js
+++ public/js/app.js
@@ -1,174 +1,198 @@
-// //
-// // DemoUnidosXFIS
-//
-//    Theme  : Firebase & Face Recognition on Real Time
-//    Author : Luis Uculmana Lara
-//             Full Stack Developer & GDG Lead
-//    Email  : ithecode.ica@gmail.com
-//
-// //
-
 // Global faces trained. Starts with empty string
-let labeledFaceDescriptors = ''
+let labeledFaceDescriptors = "";
 
 async function run() {
-
-
   // Setting up the config for load models from a URL, if you use the same models dont touch it.
-  const MODEL_URL = '/models'
+  const MODEL_URL = "/models";
 
   // Load 3 models from the url.
-  await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
-  await faceapi.loadFaceLandmarkModel(MODEL_URL)
-  await faceapi.loadFaceRecognitionModel(MODEL_URL)
-
+  await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
+  await faceapi.loadFaceLandmarkModel(MODEL_URL);
+  await faceapi.loadFaceRecognitionModel(MODEL_URL);
 
   // Making some effects for fun.
-  $(".loader").fadeOut(1000)
-  $("#send").removeAttr('disabled')
+  $(".loader").fadeOut(1000);
+  $("#send").removeAttr("disabled");
 
   // try to access users webcam and stream the images
   // to the video element
-  const input = document.getElementById('inputVideo')
+  const input = document.getElementById("inputVideo");
   navigator.getUserMedia(
     { video: {} },
-    stream => input.srcObject = stream,
+    stream => (input.srcObject = stream),
     err => console.error(err)
-  )
+  );
 
   // Now we loada Firebase Function with all saved photos in original
   // And landmarked mode with a hover effect for see it.
-  var loadPhotos = firebase.functions().httpsCallable('loadPhotos');
-  loadPhotos({}).then(function(result) {
-    // Read result of the Cloud Function.
-    var data = result.data
+  var loadPhotos = firebase.functions().httpsCallable("loadPhotos");
+  loadPhotos({})
+    .then(function(result) {
+      // Read result of the Cloud Function.
+      var data = result.data;
 
-    // Showing screenshot saved
-    result.data.forEach(data => {
-      $("#screenshots").prepend('<img src="'+ data.image_original +'" onmouseover="this.src=\''+ data.image_landmark +'\'" onmouseout="this.src=\''+ data.image_original +'\'" alt="' + data.label+ '">')
+      // Showing screenshot saved
+      result.data.forEach(data => {
+        $("#screenshots").prepend(
+          '<img src="' +
+            data.image_original +
+            '" onmouseover="this.src=\'' +
+            data.image_landmark +
+            "'\" onmouseout=\"this.src='" +
+            data.image_original +
+            '\'" alt="' +
+            data.label +
+            '">'
+        );
+      });
     })
-
-
-  }).catch((e) => {
-      console.log(e)
-  })
+    .catch(e => {
+      console.log(e);
+    });
 
   // When trying to take a screenshot we need save it on firestore.
-  $("form").submit((e) => {
+  $("form").submit(e => {
     // Prevent many miss clicks.
-    e.preventDefault()
+    e.preventDefault();
 
     // Calling to the video element by id
-    const input = document.getElementById('inputVideo')
+    const input = document.getElementById("inputVideo");
     // Creating canvas from face api function.
-    const canvas = faceapi.createCanvasFromMedia(input)
+    const canvas = faceapi.createCanvasFromMedia(input);
     // Calling the function for saving and process data.
-    screenshot_save(input, canvas)
+    screenshot_save(input, canvas);
 
-
-    return false
-  })
-
+    return false;
+  });
 
   // We need get all face descriptors after training on Firebase function
-  var processor = firebase.functions().httpsCallable('processor');
+  var processor = firebase.functions().httpsCallable("processor");
   processor({}).then(function(result) {
     // Read result of the Cloud Function.
-    var result = result.data
+    var result = result.data;
 
-    labeledFaceDescriptors = result.map( fd => {
-      return new faceapi.LabeledFaceDescriptors(fd.label, fd.faceDescriptors)
-    })
-
-
-  })
-
+    labeledFaceDescriptors = result.map(fd => {
+      return new faceapi.LabeledFaceDescriptors(fd.label, fd.faceDescriptors);
+    });
+  });
 }
 
-async function drawing_faces(overlay, fullFaceDescriptions){
+async function drawing_faces(overlay, fullFaceDescriptions) {
   // This functions is for face landmark and detection and be reused.
 
   //Face Api Drawing Detections in a box.
-  faceapi.draw.drawDetections(overlay, fullFaceDescriptions, { lineWidth: 4, color: 'blue' })
+  faceapi.draw.drawDetections(overlay, fullFaceDescriptions, {
+    lineWidth: 4,
+    color: "blue"
+  });
   //Face Api Drawing Landmarks in the face.
-  faceapi.draw.drawFaceLandmarks(overlay, fullFaceDescriptions, { lineWidth: 4, color: 'red' })
+  faceapi.draw.drawFaceLandmarks(overlay, fullFaceDescriptions, {
+    lineWidth: 4,
+    color: "red"
+  });
 }
-async function screenshot_save(input, canvas){
-
+async function screenshot_save(input, canvas) {
   // Making effects
-  $("#send").text('Sending 3 photos for training model...').attr('disabled')
+  $("#send")
+    .text("Sending 3 photos for training model...")
+    .attr("disabled");
 
   // We need a label name for the face.
-  var label = $('#label').val()
+  var label = $("#label").val();
 
   // Create base64 from screenshot from the canvas receipt
-  var dataURI = canvas.toDataURL('image/png')
+  var dataURI = canvas.toDataURL("image/png");
 
   // Taking sizes from input receipt.
-  const displaySize = { width: input.offsetWidth, height: input.offsetHeight }
+  const displaySize = { width: input.offsetWidth, height: input.offsetHeight };
   // Calling a Firebase Function for saving and process photo.
-  var savingPhoto = firebase.functions().httpsCallable('savingPhoto');
-  savingPhoto({label: label, photo: dataURI, displaySize: displaySize}).then(function(result) {
-    // Read result of the Cloud Function.
-    var data = result.data
+  var savingPhoto = firebase.functions().httpsCallable("savingPhoto");
+  savingPhoto({ label: label, photo: dataURI, displaySize: displaySize }).then(
+    function(result) {
+      // Read result of the Cloud Function.
+      var data = result.data;
 
-    // Showing screenshot saved
-    $("#screenshots").prepend('<img src="'+ data.image_original +'" onmouseover="this.src=\''+ data.image_landmark +'\'" onmouseout="this.src=\''+ data.image_original +'\'" alt="' + data.label+ '">')
+      // Showing screenshot saved
+      $("#screenshots").prepend(
+        '<img src="' +
+          data.image_original +
+          '" onmouseover="this.src=\'' +
+          data.image_landmark +
+          "'\" onmouseout=\"this.src='" +
+          data.image_original +
+          '\'" alt="' +
+          data.label +
+          '">'
+      );
 
-    // Making effects
-    $("#send").text('Train Model').removeAttr('disabled')
+      // Making effects
+      $("#send")
+        .text("Train Model")
+        .removeAttr("disabled");
 
-    return true
+      return true;
+    }
+  );
 
-  })
-
-  return false
+  return false;
 }
 
-
 async function onPlay(videoEl) {
-
   // Calling to the video element by id
-  const input = document.getElementById('inputVideo')
+  const input = document.getElementById("inputVideo");
   // Calling to the canvas element by id
-  const overlay = document.getElementById('overlay')
-
+  const overlay = document.getElementById("overlay");
 
   //Configuring the width and the height of the video.
-  const displaySize = { width: input.offsetWidth, height: input.offsetHeight }
+  const displaySize = { width: input.offsetWidth, height: input.offsetHeight };
 
   //Face Api have a match Dimensions for drawing functions.
-  faceapi.matchDimensions(overlay, displaySize)
+  faceapi.matchDimensions(overlay, displaySize);
 
   // Detecting all faces.
-  let fullFaceDescriptions = await faceapi.detectAllFaces(videoEl).withFaceLandmarks().withFaceDescriptors()
-  fullFaceDescriptions = faceapi.resizeResults(fullFaceDescriptions, displaySize)
-
+  let fullFaceDescriptions = await faceapi
+    .detectAllFaces(videoEl)
+    .withFaceLandmarks()
+    .withFaceDescriptors();
+  fullFaceDescriptions = faceapi.resizeResults(
+    fullFaceDescriptions,
+    displaySize
+  );
 
   // Drawing faces by a condition.
-  if(labeledFaceDescriptors !== ''){
+  if (labeledFaceDescriptors !== "") {
     // If the faces are charged by processor photos
     // We'll do a match the face with the video and we show a box with a label.
-    const maxDescriptorDistance = 0.6
-    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance)
+    const maxDescriptorDistance = 0.6;
+    const faceMatcher = new faceapi.FaceMatcher(
+      labeledFaceDescriptors,
+      maxDescriptorDistance
+    );
 
     // Making a map with the best result for each face descriptor.
-    const results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor))
+    const results = fullFaceDescriptions.map(fd =>
+      faceMatcher.findBestMatch(fd.descriptor)
+    );
 
     // For each better face match we gonna show a box.
     results.forEach((bestMatch, i) => {
-      const box = fullFaceDescriptions[i].detection.box
-      const text = bestMatch.toString()
-      const drawBox = new faceapi.draw.DrawBox(box, { label: text })
-      drawBox.draw(overlay)
-    })
-
-  }else{
+      const box = fullFaceDescriptions[i].detection.box;
+      const text = bestMatch.toString();
+      const drawBox = new faceapi.draw.DrawBox(box, { label: text });
+      drawBox.draw(overlay);
+    });
+  } else {
     // If dont have the face descriptors we'll do the face landmarks and detections.
-    faceapi.draw.drawDetections(overlay, fullFaceDescriptions, { lineWidth: 4, color: 'blue' })
-    faceapi.draw.drawFaceLandmarks(overlay, fullFaceDescriptions, { lineWidth: 4, color: 'red' })
+    faceapi.draw.drawDetections(overlay, fullFaceDescriptions, {
+      lineWidth: 4,
+      color: "blue"
+    });
+    faceapi.draw.drawFaceLandmarks(overlay, fullFaceDescriptions, {
+      lineWidth: 4,
+      color: "red"
+    });
   }
 
   // For each frame in the video webcam we use the function.
-  setTimeout(() => onPlay(videoEl))
+  setTimeout(() => onPlay(videoEl));
 }
